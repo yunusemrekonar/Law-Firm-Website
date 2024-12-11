fa2b57264688281b78afb339467f25f5fe564d58
yunusemrekonar <ynsemrknr@hotmail.com>
2024-12-11 10:53:44 +0300

    style upd.

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,13 @@ import './Home.css';
 const Home = () => {
   return (
     <section id="home" className="home">
-      
+   
+  {/*   <p className='home1'>Konar Hukuk & Danışmanlık Olarak,</p>*/}
+  {/* <p className='home2'><strong>Adaleti Savunma Yolunda Her Adımda Yanınızdayız</strong></p>*/}
+  { /* <p className='home3'>Güven, çözüm ve başarıyı birlikte inşa ediyoruz.</p> */}
+     
     </section>
+    
   );
 };
 
