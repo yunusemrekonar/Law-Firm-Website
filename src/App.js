3568110a2806d5f6c54b8a083e948dd6c3a2bcf4
yunusemrekonar <ynsemrknr@hotmail.com>
2024-12-15 15:44:26 +0300

    udate gitignore

--- src/App.js
+++ src/App.js
@@ -19,14 +19,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const App = () => {
   return (
     <Router>
-      <div>
         <Helmet>
           <meta name="description" content="Ankara Konar Hukuk ve Danismanlik olarak, hukuki danismanlik ve avukatlik hizmetleri sunuyoruz. Aile hukuku, ceza hukuku ve daha fazlasi için bize başvurun." />
           <meta name="keywords" content=" Ankara avukat, Ankara boşanma avukati, ceza hukuku avukati ankara, aile hukuku avukati ankara, iş hukuku avukati ankara, ticaret hukuku avukati ankara, icra ve iflas hukuku avukati ankara, gayrimenkul hukuku avukati ankara, vergi hukuku avukati ankara, miras hukuku avukati ankara, saglik hukuku avukati ankara, fikri mülkiyet hukuku avukati ankara, uluslararasi hukuk avukati ankara, avukatlik hizmetleri avukati ankara" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <title>Ankara Konar Hukuk & Danışmanlık</title>
         </Helmet>
-
+        
+        <div>
         <Navbar />
         <Routes>
           <Route
